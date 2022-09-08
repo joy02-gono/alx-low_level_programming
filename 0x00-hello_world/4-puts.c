c837ea2c201455a1b41c694351493f825d836326
rejoice <rejoicegono@gmail.com>
2022-09-08 07:03:37 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * in the main
+ * using puts
+ * printing the string programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
