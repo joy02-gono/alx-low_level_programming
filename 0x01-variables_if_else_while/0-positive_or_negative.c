e41a5522a378d1f21b664db894a5c5bbce04832c
rejoice <rejoicegono@gmail.com>
2022-09-09 10:41:24 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(v0id)
+	int n = 0
+{
+	if (n < 0)
+	{
+		printf("%d is negative\n",n);
+	} else if (n > 0)
+	{
+		print("%d is positive\n",n);
+	} else 
+	{
+		print("%d is zero\n",n);
+	}
+	return (0);
+}
