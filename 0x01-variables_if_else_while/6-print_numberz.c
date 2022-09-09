7e6d4714aff334c175c1f6829719a33c395d205d
rejoice <rejoicegono@gmail.com>
2022-09-09 13:57:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Prints the numbers since 0 to 9
+ *
+ * Description: this program prints numbers of base 10 from 0
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
