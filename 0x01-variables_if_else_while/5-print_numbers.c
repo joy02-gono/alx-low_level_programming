7213af5089d0a38ffac31cbf33b164a5fa61cb5e
rejoice <rejoicegono@gmail.com>
2022-09-09 13:49:31 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the numbers since 0 to 9
  *
- * Description: this program prints all single digit numbers of base 10 starting from 0
+ * Description: this program prints single digit of base 10 starting from 0
  *
  * Return: Always (Success)
  */
