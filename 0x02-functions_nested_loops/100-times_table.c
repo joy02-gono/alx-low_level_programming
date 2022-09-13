536170dd3a1e7aeba312d9a3d8707ebaeb87913e
rejoice <rejoicegono@gmail.com>
2022-09-13 13:01:07 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * number - function that print number
+ * @i: number to print
+ * Return: void
+ */
+
+void number(int i)
+{
+	if (i / 100 != 0)
+	{
+		_putchar(i / 100 + '0');
+		_putchar(((i % 100) / 10) + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		if (i / 10 == 0)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(i / 10 + '0');
+		}
+	}
+	_putchar(i % 10 '0');
+}
