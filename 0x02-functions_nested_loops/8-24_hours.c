d6934305e5381f0c01c57fdfcede1d4273c63c21
rejoice <rejoicegono@gmail.com>
2022-09-13 11:55:08 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_baver - func that prints every minute of the day of Jack baver,starti
+ * jack_bauer - func that prints every minute of the day of Jack bauer,starti
  * n from 00:00 to 23:59, n in loop counts mins,while hour loop counts hours
  * and resets mins
  * Return: 0
