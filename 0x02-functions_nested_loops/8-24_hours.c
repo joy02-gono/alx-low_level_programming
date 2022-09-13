b72d0f074e2e536f04240e341986cb8c5e1acf1c
rejoice <rejoicegono@gmail.com>
2022-09-13 12:46:39 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,26 @@
 #include "main.h"
+
 /**
- * jack_bauer - func that prints every minute of the day of jack bauer,starti
- * n from 00:00 to 23:59, n in loop counts mins,while hour loop counts hours
- * and resets mins
- * Return: 0
+ * jack_bauer - function that prints every minute of the day
+ *
+ * Return: void
  */
+
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+	int h;
+	int m;
 
-	while (hours <= 23)
+	for (h = 0; h < 24; h++)
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
-	}
-	hours++;
-	minutes = 0;
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar (':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
 	}
 }
