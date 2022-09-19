b837fefdec6a37a3a46c442cd76344bdfced096b
rejoice <rejoicegono@gmail.com>
2022-09-19 09:07:01 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,7 +4,7 @@
 /**
  * print_rev - Prints a string in reverse
  * @: The string to print
- *
+ * Description: function that prints a string, in reverse
  * Return: void
  */
 void print_rev(char *s)
