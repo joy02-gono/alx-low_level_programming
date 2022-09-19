dc5d182e3866996fb673774274550ae58387cd40
rejoice <rejoicegono@gmail.com>
2022-09-19 15:42:48 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,42 +2,42 @@
 
 
 /**
- * rev - function that reverse swap two char
- * @a: first parameter
- * @b: second parameter
- * Return: void
- */
+* rev - function that reverse swap two char
+* @a: first parameter
+* @b: second parameter
+* Return: void
+*/
 
 void rev(char *a, char *b)
 {
-	char tmp;
+char tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
 
 /**
- * rev_string - function that reverses a string
- * @s: string to reverse
- * Return: void
- */
+* rev_string - function that reverses a string
+* @s: string to reverse
+* Return: void
+*/
 
 void rev_string(char *s)
 {
-	int i, j;
+int i, j;
 
-	i = 0;
-	while (*(s + i))
-	{
-		i++;
-	}
-	i--;
-	j = 0;
-	while (i > j)
-	{
-		rev(s + i, s + j);
-		j++;
-		j--;
-	}
+i = 0;
+while (*(s + i))
+{
+i++;
+}
+i--;
+j = 0;
+while (i > j)
+{
+rev(s + i, s + j);
+j++;
+j--;
+}
 }
