f52b0060b31382b676dd178b08a1d57ba9d8be91
rejoice <rejoicegono@gmail.com>
2022-09-21 13:09:50 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: array to reverse
+ * @n: size of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j;
+	int k;
+
+	for (j = 0; j < n / 2; j++)
+	{
+		k = a[j];
+		a[j] = a[n - j - 1];
+		a[n -j - 1] = k;
+	}
+}
