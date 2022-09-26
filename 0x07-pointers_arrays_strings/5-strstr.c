7800b65be62054d677b323ebb1c30751b58dabe1
rejoice <rejoicegono@gmail.com>
2022-09-26 11:13:08 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack or NULL if no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int j = 0, k = 0;
+
+	while (haystack[j])
+	{
+		while (needle[k] && (haystack[j] == needle[0]))
+		{
+			if (haystack[j + k] == needle[k])
+				k++;
+			else
+				break;
+		}
+		if (needle[k])
+		{
+			j++;
+			k = 0;
+		}
+		else
+		{
+			return (haystack + j);
+		}
+	}
+	return (0);
+}
