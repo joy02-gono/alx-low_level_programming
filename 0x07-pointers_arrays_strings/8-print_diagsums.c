a116322c172ca4d242494962d1e25273ed05b1c6
rejoice <rejoicegono@gmail.com>
2022-09-26 12:05:01 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,17 +6,21 @@
  * @a: pointer to start of matrix
  * @size: witdth of matrix colum
  *
- * Return void
+ * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0
+	int i, j, p, l = 0, r = 0;
 
-		for (i = 0; i < size; i++)
-		{
-			sum1 += a[(size + 1) * i];
-			sum2 += a[(size - 1) * (i + 1)];
-		}
-
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
