10337c1ad525863876125b111e77a12849ea53bf
rejoice <rejoicegono@gmail.com>
2022-09-26 11:16:00 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,6 +4,8 @@
  * _strstr - locates a substring
  * @haystack: string in which to check for needle
  * @needle: substring to find in haystack or NULL if no match
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
 {
