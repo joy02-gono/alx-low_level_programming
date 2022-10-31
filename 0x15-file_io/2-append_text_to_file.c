5e9aa30e8ae94674a773fa61588fba4547a669f1
rejoice <rejoicegono@gmail.com>
2022-10-31 09:46:40 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: pointer to nam eof file
+ * @filename: pointer to name of file
  * @text_content: null terminated string
  *
  * Return: 1 on success, -1 on failure
