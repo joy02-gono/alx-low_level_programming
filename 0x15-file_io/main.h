a9bb33d3b5239046dc91a31dbc2ebed274e9c954
rejoice <rejoicegono@gmail.com>
2022-10-31 09:20:28 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* library files*/
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -8,8 +10,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+/*function prototypes*/
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN_H */
