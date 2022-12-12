f0dcf8f00969cb76ce08446dc465493e186f1615
rejoice <rejoicegono@gmail.com>
2022-12-12 15:26:07 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+wget -P /tmp https://github.com/joy02-gono/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
